ad9b7bdc184d996254ebbe170bb243cd505de8f7
Luca Galasso <galasso.luca@outlook.com>
2017-11-23 10:31:42 +0100

    wrong class name

--- mt32.rb
+++ mt32.rb
@@ -1,6 +1,6 @@
 require_relative 'personal-mqtt'
 
-class EM32 < MyMqtt
+class MT32 < MyMqtt
   attr_accessor :pub_topic
 
   def initialize (patient_id, session_id)
@@ -12,12 +12,12 @@ end
 
 
 
-patient1 = EM32.new("patient1", "sessionXXX")
+patient1 = MT32.new("patient1", "sessionXXX")
 patient1.publish("First message")
 patient1.publish("Second message")
 patient1.publish("Third message")
 
-patient2 = EM32.new("patient2", "sessionXXX")
+patient2 = MT32.new("patient2", "sessionXXX")
 patient2.publish("First message")
 patient2.publish("Second message")
 patient2.publish("Third message")
